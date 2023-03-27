2ee8ba3f66056832c26d560d582ca89e3d732123
KristenSantos <Kristensxntos@gmail.com>
2023-03-27 19:34:27 -0400

    Added pictures

--- script.js
+++ script.js
@@ -6,21 +6,39 @@ const options = {
 	}
 };
 
-
 window.addEventListener('DOMContentLoaded', async() => {
-
     const popularDes = document.getElementById('popular-des')
+    const popularDes2 = document.getElementById('popular-des2')
     let response = await fetch('https://travel-advisor.p.rapidapi.com/locations/search?query=popular%20location&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US', options)
     let user = await response.json()
     console.log(user);
-        for(let i = 0; i < 4; i++){
-        const link = document.createElement('a');
-        link.innerText = user.data[i].result_object.address
-        const list = document.createElement('li');
-        list.append(link)
-        popularDes.append(list)
-
+        for(let i = 0; i < 2; i++){
+        const pic = document.createElement('img');
+        const caption = document.createElement('figcaption')
+        pic.src = user.data[i].result_object.photo.images.small.url
+        caption.innerText = user.data[i].result_object.address
+        const frame = document.createElement('div');
+        frame.append(pic)
+        frame.append(caption)
+        popularDes.append(frame)
+        popularDes.append(caption)
+    }
+    for(let i = 2; i < 4; i++){
+        const pic = document.createElement('img');
+        const caption = document.createElement('figcaption')
+        pic.src = user.data[i].result_object.photo.images.small.url
+        caption.innerText = user.data[i].result_object.address
+        const frame = document.createElement('div');
+        frame.append(pic)
+        frame.append(caption)
+        popularDes2.append(frame)
+        popularDes2.append(caption)
     }
 });
 
+document.querySelector("#search").addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const searchTerm = e.target[0].value
+    console.log(searchTerm)
 
+})
