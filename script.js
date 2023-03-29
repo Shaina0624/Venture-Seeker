747df31607f287ffa76870a928325e74b0e0b1e9
KristenSantos <Kristensxntos@gmail.com>
2023-03-29 17:50:12 -0400

    fixed

--- script.js
+++ script.js
@@ -101,27 +101,5 @@ function setAttrations(user3, user4){
     }
 }
 
-$('#recipeCarousel').carousel({
-    interval: 10000
-  })
-  
-  $('.carousel .carousel-item').each(function(){
-      var minPerSlide = 3;
-      var next = $(this).next();
-      if (!next.length) {
-      next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
-      
-      for (var i=0;i<minPerSlide;i++) {
-          next=next.next();
-          if (!next.length) {
-              next = $(this).siblings(':first');
-            }
-          
-          next.children(':first-child').clone().appendTo($(this));
-        }
-  });
-
 
 
