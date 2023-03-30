6b0663888c17594c5d692fef847e10e23eea2230
KristenSantos <Kristensxntos@gmail.com>
2023-03-30 18:57:03 -0400

    idk

--- config.js
+++
@@ -1,7 +0,0 @@
-const options = {
-	method: 'GET',
-	headers:{
-		'X-RapidAPI-Key': '2df429e771msh05586fa7de4000dp18bf10jsne610e6ce195a',
-		'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-	}
-};
