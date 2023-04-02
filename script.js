39e1dc23599461c30814710b843b5ccdccc77cf0
KristenSantos <Kristensxntos@gmail.com>
2023-04-01 23:29:52 -0400

    fixed cards

--- script.js
+++ script.js
@@ -1,135 +1,218 @@
 const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "ce00b6d8bcmshf06e2c73f313135p1a6f4bjsndff080f47c4a",
-      "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-    },
-  };
-  
-  const options2 = {
-    method: "GET",
-    headers: {
-      Authorization: "KqSKv5wC2NI1jyqbOY8h2gMHMYSdGAn5PIqopw2FRhUifbs4VMAtWBOE",
-    },
-  };
-  
-  window.addEventListener("DOMContentLoaded", async () => {
-    fetchLocation();
-  });
-  
-  async function fetchLocation() {
-    const popularDes = document.getElementById("popular-des");
-    let response = await fetch(
-      "https://travel-advisor.p.rapidapi.com/locations/search?query=popular%20location&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US",
-      options
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cf20d356f0msh262851abf2aedd2p1d2d60jsn7e0a76a1272e",
+    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+  },
+};
+
+const options2 = {
+  method: "GET",
+  headers: {
+    Authorization: "KqSKv5wC2NI1jyqbOY8h2gMHMYSdGAn5PIqopw2FRhUifbs4VMAtWBOE",
+  },
+};
+//////////////////////////////
+///Variables for activities//
+////////////////////////////
+const activity1 = document.getElementById("activitiesTitle1");
+const activityImg1 = document.getElementById("activitiesImg1");
+const activitiyDescription1 = document.getElementById("activitiyDescription1");
+
+const activity2 = document.getElementById("activitiesTitle2");
+const activityImg2 = document.getElementById("activitiesImg2");
+const activitiyDescription2 = document.getElementById("activitiyDescription2");
+
+const activity3 = document.getElementById("activitiesTitle3");
+const activityImg3 = document.getElementById("activitiesImg3");
+const activitiyDescription3 = document.getElementById("activitiyDescription3");
+
+//////////////////////////////
+///Variables for resturant///
+////////////////////////////
+const resturantImg1 = document.getElementById("resturantImg1");
+const resturantTitle1 = document.getElementById("resturantTitle1");
+const resturantDescription1 = document.getElementById("resturantDescription1");
+
+const resturantImg2 = document.getElementById("resturantImg2");
+const resturantTitle2 = document.getElementById("resturantTitle2");
+const resturantDescription2 = document.getElementById("resturantDescription2");
+
+const resturantImg3 = document.getElementById("resturantImg3");
+const resturantTitle3 = document.getElementById("resturantTitle3");
+const resturantDescription3 = document.getElementById("resturantDescription3");
+
+window.addEventListener("DOMContentLoaded", async () => {
+  fetchLocation();
+});
+
+async function fetchLocation() {
+  const popularDes = document.getElementById("popular-des");
+  let response = await fetch(
+    "https://travel-advisor.p.rapidapi.com/locations/search?query=popular%20location&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US",
+    options
+  );
+  let user = await response.json();
+  let locations = [];
+  for (let i = 0; i < 4; i++) {
+    locations.push(user.data[i].result_object.address);
+  }
+  console.log(locations);
+  pexelFetch(locations);
+}
+
+async function pexelFetch(locations) {
+  let pictures = [];
+  for (let i = 0; i < locations.length; i++) {
+    let response2 = await fetch(
+      `https://api.pexels.com/v1/search?query=${locations[i]}`,
+      options2
     );
-    let user = await response.json();
-    let locations = [];
-    for (let i = 0; i < 4; i++) {
-      locations.push(user.data[i].result_object.address);
-    }
-    console.log(locations);
-    pexelFetch(locations);
+    let user2 = await response2.json();
+    console.log(user2);
+    pictures.push(user2.photos[3].src.landscape);
   }
-  
-  async function pexelFetch(locations) {
-    let pictures = [];
-    for (let i = 0; i < locations.length; i++) {
-      let response2 = await fetch(
-        `https://api.pexels.com/v1/search?query=${locations[i]}`,
-        options2
-      );
-      let user2 = await response2.json();
-      console.log(user2);
-      pictures.push(user2.photos[3].src.landscape);
-    }
-    displayLocations(locations, pictures);
+  displayLocations(locations, pictures);
+}
+
+function displayLocations(location, pictures) {
+  for (let i = 0; i < location.length; i++) {
+    const popularDes = document.getElementById("popular-des");
+    const pic = document.createElement("img");
+    const caption = document.createElement("figcaption");
+    pic.src = pictures[i];
+    pic.style.width = "300px";
+    pic.style.height = "300px";
+    pic.style.borderRadius = "5px";
+    caption.innerText = location[i];
+    const frame = document.createElement("div");
+    frame.append(pic);
+    frame.append(caption);
+    popularDes.append(frame);
+    popularDes.append(caption);
   }
-  
-  function displayLocations(location, pictures) {
-    for (let i = 0; i < location.length; i++) {
-      const popularDes = document.getElementById("popular-des");
-      const pic = document.createElement("img");
-      const caption = document.createElement("figcaption");
-      pic.src = pictures[i];
-      pic.style.width = "300px";
-      pic.style.height = "300px";
-      pic.style.borderRadius = "5px";
-      caption.innerText = location[i];
-      const frame = document.createElement("div");
-      frame.append(pic);
-      frame.append(caption);
-      popularDes.append(frame);
-      popularDes.append(caption);
-    }
-  }
-  
-  document.querySelector("form").addEventListener("submit", async (e) => {
-      e.preventDefault()
-      const searchTerm = document.querySelector("#search").value
-      console.log(searchTerm)
-      const response3 = await fetch(`https://travel-advisor.p.rapidapi.com/locations/search?query=${searchTerm}&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US`, options)
-      const response4 = await fetch(`https://api.pexels.com/v1/search?query=${searchTerm}`,options2 );
-      user3 = await response3.json();
-      user4 = await response4.json();
-      term = user3
-      console.log(user3)
-      console.log(user4)
-    //   window.location.href = "activities.html"
-      // console.log(user3.data[2].result_type)
-      setAttrations(user3, user4);
-  })
-  
-  function setAttrations(user3, user4) {
-    const picture = document.getElementById("image");
-    const locationName = document.getElementById("locationName");
-    const locationsDescription = document.getElementById("locationsDescription");
-    const activity1 = document.getElementById("activity1");
-    const activity2 = document.getElementById("activity2");
-    const activity3 = document.getElementById("activity3");
-    const accommodations1 = document.getElementById("accommodations1");
-    const accommodations2 = document.getElementById("accommodations2");
-    const accommodations3 = document.getElementById("accommodations3");
-    const restaurants1 = document.getElementById("restaurants1");
-    const restaurants2 = document.getElementById("restaurants2");
-    const restaurants3 = document.getElementById("restaurants3");
-    picture.src = user4.photos[0].src.large;
-    picture.style.paddingLeft = "100px";
-    picture.style.height = "500px";
-    picture.style.width = "700px";
-    let name = user3.data[0].result_object.location_string.toUpperCase();
-    locationName.innerText = name;
-    locationsDescription.innerText = user3.data[0].result_object.geo_description;
-  
-    // save your data in the browser
-    //   window.localStorage.setItem('data', JSON.stringify({foo: 'bar'}))
-  
-    //   // retrieve data from browser
-    //   const data = JSON.parse(window.localStorage.getItem('data'))
-  
-    for (let i = 0; i < 30; i++) {
-      if (user3.data[i].result_type === "lodging") {
-        console.log(user3.data[i].result_type);
-        accommodations1.src =
-          user3.data[i].result_object.photo.images.original.url;
-        accommodations2.src =
-          user3.data[i + 1].result_object.photo.images.original.url;
-        accommodations3.src =
-          user3.data[i + 2].result_object.photo.images.original.url;
-      } else if (user3.data[i].result_type === "restaurants") {
-        console.log(user3.data[i].result_type);
-        restaurants1.src = user3.data[i].result_object.photo.images.original.url;
-        restaurants2.src =
-          user3.data[i + 1].result_object.photo.images.original.url;
-        restaurants3.src =
-          user3.data[i + 2].result_object.photo.images.original.url;
-      } else if (
-        user3.data[i].result_type === "things_to_do" ||
-        user3.data[i].result_type === "activities"
-      ) {
-        activity1.src = user3.data[i].result_object.photo.images.original.url;
-        activity2.src = user3.data[i + 1].result_object.photo.images.original.url;
-        activity3.src = user3.data[i + 2].result_object.photo.images.original.url;
-      }
+}
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const searchTerm = document.querySelector("#search").value;
+  console.log(searchTerm);
+  const response3 = await fetch(
+    `https://travel-advisor.p.rapidapi.com/locations/search?query=${searchTerm}&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US`,
+    options
+  );
+  const response4 = await fetch(
+    `https://api.pexels.com/v1/search?query=${searchTerm}`,
+    options2
+  );
+  user3 = await response3.json();
+  user4 = await response4.json();
+  console.log(user3);
+  console.log(user4);
+  setAttrations(user3, user4);
+});
+
+function setAttrations(user3, user4) {
+  const picture = document.getElementById("image");
+  const locationName = document.getElementById("locationName");
+  const locationDescription = document.getElementById("locationDescription");
+  picture.src = user4.photos[0].src.original;
+  let name = user3.data[0].result_object.location_string.toUpperCase();
+  locationName.innerText = name;
+  locationName.append(locationDescription);
+  locationDescription.innerHTML = user3.data[0].result_object.geo_description;
+  const activitiesAndRestaurants = new Set();
+
+  for (let i = 0; i < 30; i++) {
+    if (
+      (activitiesAndRestaurants.has(user3.data[i]) === false &&
+        user3.data[i].result_type === "things_to_do") ||
+      user3.data[i].result_type === "activities"
+    ) {
+      activityImg1.src = user3.data[i].result_object.photo.images.original.url;
+      activity1.innerText = user3.data[i].result_object.name;
+      activitiyDescription1.innerText = `Location:${user3.data[i].result_object.location_string}
+          Rating:${user3.data[i].result_object.rating}`;
+      activitiesAndRestaurants.add(user3.data[i]); // add the actual object to the Set
+      break;
     }
   }
+
+  for (let i = 0; i < 30; i++) {
+    if (
+      (activitiesAndRestaurants.has(user3.data[i]) === false &&
+        user3.data[i].result_type === "things_to_do") ||
+      user3.data[i].result_type === "activities"
+    ) {
+      activityImg2.src = user3.data[i].result_object.photo.images.original.url;
+      activity2.innerText = user3.data[i].result_object.name;
+      activitiyDescription2.innerText = `Location:${user3.data[i].result_object.location_string}
+          Rating:${user3.data[i].result_object.rating}`;
+      activitiesAndRestaurants.add(user3.data[i]); // add the actual object to the Set
+      break;
+    }
+  }
+
+  for (let i = 0; i < 30; i++) {
+    if (
+      (activitiesAndRestaurants.has(user3.data[i]) === false &&
+        user3.data[i].result_type === "things_to_do") ||
+      user3.data[i].result_type === "activities"
+    ) {
+      activityImg3.src = user3.data[i].result_object.photo.images.original.url;
+      activity3.innerText = user3.data[i].result_object.name;
+      activitiyDescription3.innerText = `Location:${user3.data[i].result_object.location_string}
+          Rating:${user3.data[i].result_object.rating}`;
+      activitiesAndRestaurants.add(user3.data[i]); // add the actual object to the Set
+      break;
+    }
+  }
+
+  for (let i = 0; i < 30; i++) {
+    if (
+      user3.data[i].result_type === "restaurants" &&
+      activitiesAndRestaurants.has(user3.data[i]) === false
+    ) {
+      resturantImg1.src = user3.data[i].result_object.photo.images.original.url;
+      resturantTitle1.innerText = user3.data[i].result_object.name;
+      resturantDescription1.innerText = `Location:${user3.data[i].result_object.location_string}
+            Rating:${user3.data[i].result_object.rating}`;
+      activitiesAndRestaurants.add(user3.data[i]); // add the actual object to the Set
+      break;
+    }
+  }
+  for (let i = 0; i < 30; i++) {
+    if (
+      user3.data[i].result_type === "restaurants" &&
+      activitiesAndRestaurants.has(user3.data[i]) === false
+    ) {
+      resturantImg2.src = user3.data[i].result_object.photo.images.original.url;
+      resturantTitle2.innerText = user3.data[i].result_object.name;
+      resturantDescription2.innerText = `Location:${user3.data[i].result_object.location_string}
+            Rating:${user3.data[i].result_object.rating}`;
+      activitiesAndRestaurants.add(user3.data[i]); // add the actual object to the Set
+      break;
+    }
+  }
+  for (let i = 0; i < 30; i++) {
+    if (
+      user3.data[i].result_type === "restaurants" &&
+      activitiesAndRestaurants.has(user3.data[i]) === false
+    ) {
+      resturantImg3.src = user3.data[i].result_object.photo.images.original.url;
+      resturantTitle3.innerText = user3.data[i].result_object.name;
+      resturantDescription3.innerText = `Location:${user3.data[i].result_object.location_string}
+            Rating:${user3.data[i].result_object.rating}`;
+      activitiesAndRestaurants.add(user3.data[i]); // add the actual object to the Set
+      break;
+    }
+  }
+
+  console.log(activitiesAndRestaurants);
+}
+
+// save your data in the browser
+//   window.localStorage.setItem('data', JSON.stringify({foo: 'bar'}))
+
+//   // retrieve data from browser
+//   const data = JSON.parse(window.localStorage.getItem('data'))
